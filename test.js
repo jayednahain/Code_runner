083d6411dacd7758d0c4a42e0296cbfdeb7117a6
jayed.nahian <jayed.nahian@trucklagbe>
2023-11-13 13:56:04 +0600

    solve_isAnagram_again

--- test.js
+++ test.js
@@ -704,32 +704,46 @@ var twoSum = function(nums, target) {
     }
 };
 
-var isAnagram = function(s, t) {
+var isAnagram = function (s, t) {
     if (s.length !== t.length) {
-        return false; // If the lengths are different, they can't be anagrams
+        return false;
     }
-
-    const charCount = {};
-
+    const table = {};
     for (let char of s) {
-        console.log(charCount)
-        charCount[char] = (0 || 0) + 1;
+        table[char] = (table[char] || 0) + 1;
+        console.log(table)
     }
+    console.log("____________")
 
-    // for (let char of t) {
-    //     if (!charCount[char]) {
-    //         return false; // If the character is not in the s string
-    //     }
-    //     charCount[char] -= 1;
-    // }
-
-    // return true; 
+    for (let i of t) {
+        
+        if (table[i] == undefined) {
+            return false
+        }
+        table[i] = table[i] - 1;
+        if (table[i] < 0) {
+            return false
+        }
+        console.log(table)
+    }
+    return true;
 };
 
 
+findFrequency = (words, letter) => {
+    let table = {};
+    for (let i of words) {
+        table[i] = (table[i] || 0) + 1
+        
+    }
+    return table[letter]
+}
 
 
-try{
-	console.log(isAnagram('anagram','nagaram'))
+
+try {
+    // findFrequency("JSESSSON")
+    // console.log(findFrequency("JSESSSSON","S"))
+	console.log(isAnagram("aacc", 'ccac'))
 }
 catch(e){console.log(e.message)}
