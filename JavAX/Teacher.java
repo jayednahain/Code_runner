41927200ac959b65ad06d9e65862d52e1040d798
jayednahian <jayednahian@yahoo.com>
2024-03-16 00:56:18 +0600

    created method with parameter

--- JavAX/Teacher.java
+++ JavAX/Teacher.java
@@ -2,9 +2,17 @@ public class Teacher {
     String name, gender;
     int phoneNumber;
 
+
+    void getInformation (String n, String g, int number){
+        name = n;
+        gender = g;
+        phoneNumber = number;
+    }
+
     void displayPhoneNumber(){
         System.out.println("name: " +name);
         System.out.println("gender: " +gender);
         System.out.println("gender: " + phoneNumber);
+        System.out.println("---------------------------");
     }
 }
