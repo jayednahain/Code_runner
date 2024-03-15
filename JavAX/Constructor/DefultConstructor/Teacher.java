dbe9dab74fc30de47c38e25223b1ccb9b3a23c6f
jayednahian <jayednahian@yahoo.com>
2024-03-16 01:53:34 +0600

    defult constructor

---
+++ JavAX/Constructor/DefultConstructor/Teacher.java
@@ -0,0 +1,20 @@
+package Constructor.DefultConstructor;
+public class Teacher {
+
+    String name, gender;
+    int phoneNumber;
+
+    // 
+    Teacher(){
+        
+    }
+
+   
+
+    void displayInformation(){
+        System.out.println("name: " +name);
+        System.out.println("gender: " +gender);
+        System.out.println("gender: " + phoneNumber);
+        System.out.println("---------------------------");
+    }
+}
