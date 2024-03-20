e10d989dd2bf37d11268244dec5f3d684f1dcc4c
jayednahian <jayednahian@yahoo.com>
2024-03-21 03:22:02 +0600

    219: running leet code

---
+++ JavAX/LeetCode/Problems.java
@@ -0,0 +1,38 @@
+package LeetCode;
+
+public class Problems {
+    //https://leetcode.com/problems/contains-duplicate/description/
+    //Brute Force Approach
+    // boolean containsDuplicate(int[] nums) {
+    //     for (int i = 0; i < nums.length; i++) {
+    //         System.out.println(nums[i]);
+    //         for (int j = i+1; j < nums.length; j++) {
+    //             if (nums[i] == nums[j])
+    //             {
+    //                 return true;
+    //             }
+    //             System.out.println("j ==" + nums[j]);
+    //         }
+    //     }
+    //     return false;
+    // }
+
+    // boolean containsDuplicateUsingSort(int[] nums) {
+    //     Arrays.sort(nums);
+    //     for (int i = 0; i < nums.length-1; i++) {
+    //       System.out.println(nums[i]);
+    //       if (nums[i] == nums[i+1]){
+    //         return true;
+    //       }
+    //     }
+    //     return false;
+    // }
+        // 219
+    // https://leetcode.com/problems/contains-duplicate-ii/description/
+
+    public void containsNearbyDuplicate(int[] nums, int k) {
+
+
+        
+    }
+}
---
+++ JavAX/LeetCode/Test.java
@@ -0,0 +1,18 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Test obj = new Test();
+        // obj.containsDuplicate(test);
+
+        int[] test = {1,1,2,3,4,5,6};
+        // boolean containsDuplicate = obj.containsDuplicateUsingSort(test);
+    }
+
+    
+
+
+}
