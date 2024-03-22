42faab4f15d55fcefb69430f0222bf79812748e1
jayednahian <jayednahian@yahoo.com>
2024-03-22 20:35:42 +0600

    109: complete: static variable example

---
+++ JavAX/StaticUse/StaticVariable/Ex3_object_counting/Test.java
@@ -0,0 +1,17 @@
+package StaticUse.StaticVariable.Ex3_object_counting;
+//111 playlist
+public class Test {
+    public static void main(String[] args) {
+
+        Student obj1 = new Student();
+        obj1._getCurrentCount();
+        Student obj2 = new Student();
+        obj2._getCurrentCount();
+        Student obj3 = new Student();
+        obj3._getCurrentCount();
+        Student obj4 = new Student();
+        obj4._getCurrentCount();
+
+        // obj1._getCurrentCount();
+    }
+}
