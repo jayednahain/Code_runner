3ed4c5f3eacad87f079af86e50120fe2b4cffacb
jayednahian <jayednahian@yahoo.com>
2024-03-26 14:19:29 +0600

    method overloading move to overloading folder

---
+++ JavAX/Overloading/MethodOverLoading/Test.java
@@ -0,0 +1,10 @@
+package Overloading.MethodOverLoading;
+
+
+public class Test {
+    public static void main(String[] args) {
+        MethodOverloadingUse obj1 = new MethodOverloadingUse();
+        obj1.add("a","b");
+        obj1.add(4,5);
+    }
+}
