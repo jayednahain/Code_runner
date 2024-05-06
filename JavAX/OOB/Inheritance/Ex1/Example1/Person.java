cac69c4013ab00062675bc85bab02389995eb3ac
jayed.nahian <jayed.nahian@trucklagbe>
2024-05-06 09:58:43 +0600

    inheritance move to OOB

---
+++ JavAX/OOB/Inheritance/Ex1/Example1/Person.java
@@ -0,0 +1,16 @@
+package Jav128InheritancePratice.Example1;
+
+public class Person {
+    String name;
+    int age;
+
+    Person() {
+        System.out.println("|| Person  || init -");
+    }
+
+    void displayPersonInfo() {
+        System.out.println("Name : " + name);
+        System.out.println("Age : "+ age);
+    }
+
+}
