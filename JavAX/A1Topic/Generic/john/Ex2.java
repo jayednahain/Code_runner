76a6bf5ccea22ed247bb50f760c8382b1808d58f
jayed.nahian <jayed.nahian@trucklagbe>
2024-09-15 17:44:54 +0600

    running

--- JavAX/A1Topic/Generic/john/Ex2.java
+++ JavAX/A1Topic/Generic/john/Ex2.java
@@ -1,5 +1,49 @@
 package JavAX.A1Topic.Generic.john;
 
-public class Ex2 {
 
+class Animal {
+
+}
+
+
+class Cat extends Animal {
+    Cat() {
+        System.out.println("cat class init");
+    }    
+}
+
+
+class Dog extends Animal {
+    Dog() {
+        System.out.println("Dog class init");
+    }
+}
+
+
+class Bird extends Animal {
+    Bird() {
+        System.out.println("Bird class init");
+    }
+}
+
+
+class CustomGenericType<T extends Animal> {
+    T value;
+    CustomGenericType(T value) {
+        this.value = value;
+    }
+
+    public void showGenericData() {
+        System.out.println(value);
+    }
+
+}
+
+
+public class Ex2 {
+    Ex2() {
+
+    }
+
+    CustomGenericType<Cat> customGenericTypeObj1 = new CustomGenericType(new Cat())
 }
