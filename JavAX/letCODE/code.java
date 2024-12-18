3b80e98994e08ee17f634bac02f43c3de87b6347
jayednahian <jayednahian@yahoo.com>
2024-12-19 00:02:32 +0600

    letCode code started

---
+++ JavAX/letCODE/code.java
@@ -0,0 +1,19 @@
+package JavAX.letCODE;
+
+import java.util.Arrays;
+
+public class code {
+
+    public static void main(String[] args) {
+        int[] a = {2,5,4,7};
+
+        System.out.println(Arrays.toString(a));
+
+        Arrays.sort(a);
+        System.out.println(Arrays.toString(a));
+
+        for (char i : a) {
+            
+        }
+    }
+}
