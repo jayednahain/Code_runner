4606705973fa3e54e497a229aa2324ee1b10d014
jayednahian <jayednahian@yahoo.com>
2025-03-06 16:04:31 +0600

    Solve -2 running

--- QAProblems/solve2/solve2.java
+++ QAProblems/solve2/solve2.java
@@ -1,39 +1,52 @@
 package QAProblems.solve2;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
 
 
 public class solve2 {
     public static void main(String[] args) {
-        int[] nums = {100, 4, 200, 1, 3, 2};
-
+        int[] nums = {100, 4, 200, 1, 3, 2,2,22};
         LongestConsecutiveSequence solve2Obj = new LongestConsecutiveSequence();
-        solve2Obj.longestConsecutive(nums);
-
+        // solve2Obj.longestConsecutive(nums);
         System.out.println(solve2Obj.longestConsecutive(nums)); // Output: 4
-
     }
-
 }
 
 class LongestConsecutiveSequence {
-    public static int longestConsecutive(int[] nums){
-        if (nums.length == 0) return 0;
+    public int longestConsecutive(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
 
-        HashSet<Integer> currentSet = new HashSet<>();
-        //This code transfers all elements from the array
-        // into a HashSet to enable O(1) lookup time instead of O(n)
-        // scanning for each check.
-        
+        Set<Integer> numSet = new HashSet<>();
+        //This creates a new instance of the HashSet class.
+        //The new keyword allocates memory for the HashSet object.
+        //set is interface
+        // Set is an interface, and Integer is a wrapper class for the primitive type int.
+       
         for (int num : nums) {
-            currentSet.add(num); // Store all numbers in a HashSet for O(1) lookups
+            numSet.add(num);
         }
 
         int maxLength = 0;
+        for (int num : nums) {
+            System.out.println("num :" + num);
+            if (!numSet.contains(num - 1)) { 
 
+                System.out.println("1---num :"+ (num-1));
+                int currentNum = num;
+                int count = 1;
 
-        System.out.println(currentSet.contains(200));
-
+                while (numSet.contains(currentNum + 1)) { 
+                    currentNum++;
+                    count++;
+                }
+                maxLength = Math.max(maxLength, count); 
+            }
+        }
         return maxLength;
     }
+
+
 }
