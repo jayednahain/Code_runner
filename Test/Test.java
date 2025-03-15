40a1bba9ba54c2156e5c8c28b550b7cc4c4cfb0e
TL Test User <tltestuser@TLs-MacBook-Air.local>
2025-03-15 23:39:10 +0600

    group solve -1 128

--- Test/Test.java
+++ Test/Test.java
@@ -1,8 +1,49 @@
 package Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+class ArrayWalk {
+    public void walk() {
+       // int[] numbers = {10, 20, 30, 40, 50};
+
+        //for each loop
+        // for (int number : numbers) {
+        //     System.out.println("number: " + number);
+        // }
+
+        //for loop
+        // System.out.println(Arrays.toString(numbers));
+        // for (int i = 0; i < numbers.length; i++) {
+        //     System.out.println("numbers[i]: " + numbers[i]);
+        //     numbers[i] = numbers[i] + 5;
+        // }
+        // System.out.println(Arrays.toString(numbers));
+
+
+        // // 5. Looping through a String array
+        // String[] names = {"Alice", "Bob", "Charlie"};
+        // System.out.println("Looping through a String array:");
+        // for (String name : names) {
+        //     System.out.println("Name: " + name);
+        // }
+
+        int[] array1 = {1, 2, 3, 4, 5};
+        int[] array2 = {3, 5, 6, 7, 8};
+        List<Integer> commonList = Arrays.asList();
+        for (int i = 0; i < array1.length; i++) {
+            for (int j = 0; j < array2.length; j++) {
+                if (array1[i] == array2[j]) {
+                    commonList.add(array1[i]);
+            }
+        }
+    }
+}
+
 public class Test {
     public static void main(String[] args) {
-        
+        ArrayWalk arrayWalk = new ArrayWalk();
+        arrayWalk.walk();
     }
 }
 
--- groupProblem/solve1_128.java
+++ groupProblem/solve1_128.java
@@ -1,5 +1,48 @@
 package groupProblem;
 
 public class solve1_128 {
-    
+    Solution solution = new Solution();
+    int[] nums1 = {1,2,3,0,0,0};
+    int[] nums2 = {2,5,6};
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = {2,5,6};
+
+        solution.merge(nums1, 3, nums2, 3);
+    }
+
+}
+
+
+
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int pointerOne = m -1;
+        int pointerTwo = n -1;
+        int pointerMarged = m + n -1;
+
+
+
+        for (int i = pointerMarged; i >= 0; i--) {
+            if (pointerTwo < 0) {
+                nums1[i] = nums1[pointerOne];
+                pointerOne--;
+            } else if (pointerOne < 0) {
+                nums1[i] = nums2[pointerTwo];
+                pointerTwo--;
+            } else if (nums1[pointerOne] > nums2[pointerTwo]) {
+                nums1[i] = nums1[pointerOne];
+                pointerOne--;
+            } else {
+                nums1[i] = nums2[pointerTwo];
+                pointerTwo--;
+            }
+        }
+
+
+    }
 }
